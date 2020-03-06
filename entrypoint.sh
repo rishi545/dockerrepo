14fffc9ac450d54fa650efaecd9047d519e2c388
rishi545 <rishi545@gmail.com>
2020-03-06 06:35:15 +0000

    first official v1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 if  [  "$color"  ==  "black"  ]
 then
-        cp  /opt/ashu.html   /var/www/html/index.html
+        cp  /opt/rishi.html   /var/www/html/index.html
+	cp  /opt/docker.png /var/www/html/
         httpd  -DFOREGROUND
 elif  [   "$color"  ==  "red"    ]
 then
         cp   /opt/red.html   /var/www/html/index.html
+	cp  /opt/docker.png /var/www/html/
         httpd  -DFOREGROUND
 elif  [   "$color"  ==   "green"     ]
 then
         cp   /opt/green.html   /var/www/html/index.html
+	cp  /opt/docker.png /var/www/html/
         httpd  -DFOREGROUND
 else
         echo  "Sorry no color found  "   >/var/www/html/index.html
+	cp  /opt/docker.png /var/www/html/
         httpd  -DFOREGROUND
 fi
